a14dfb5ef79cb04e12bece4b157e409bcfde2493
Vicente A <valcalde@miuandes.cl>
2021-04-13 10:27:16 -0400

    arreglado

--- assassin.py
+++ assassin.py
@@ -3,5 +3,5 @@ class Assassin(Card): #asesino listo
     def __init__(self,name):
            super().__init__(name) 
 
-    def killer(self,name_player,card):
-        self.kill=(name_player,card)
+    def killer(self,name_player):
+        self.kill=(name_player)
--- player.py
+++ player.py
@@ -1,6 +1,6 @@
 class Player():
     def __init__(self,name,coins,cards): #las cartas van a cambiar mucho, creo que no vale la pena que sean privadas 
-        self.name=name
+        self.__name=name
         self.__coins=coins
         self.__cards=cards
         self.__life=True
@@ -10,31 +10,32 @@ class Player():
         return self.__name
 
     @property
-    def coins(slef):
+    def coins(self):
         return self.__coins
 
     @property
-    def life(slef):
+    def life(self):
         return self.__life
 
     @coins.setter
-    def coins(self,value):
+    def coins(self,value,__coins):
         if (self.coins + value) < 0:
             self.__coins = 0 #ver por que esto da error
         else:
             self.__coins += value
 
     @life.setter
-    def life(self,value):
+    def life(self,value,__life):
         if self.__life == True:
              self.__life = False
 
     def raise_card(self):
         value = int(input("enter the position of the card you want to turn"))
         self.cards[value][0] = 0
-        return self.cards 
+        self.cards 
 
     def end_game(self):
+        self.life = False
         self.life = False
 
     def change_coins(self,value):
@@ -46,18 +47,18 @@ class Player():
             return 2 
         return 
     
-    '''
+
 
     @property
-    def cards(slef):
+    def cards(self):
         return self.__cards
-
+'''
     @cards.setter
     def cards(self,value):
         if (len(self.cards)<=2):
             if (value[0] == 0):
-'''
 
+'''
 
 
         
