1aadb568e022b1322b64be52e71085b5eca791c9
Vicente A <valcalde@miuandes.cl>
2021-04-20 08:02:08 -0400

    arreglos main terminados

--- main.py
+++ main.py
@@ -234,12 +234,12 @@ def main():
                             print(f"{x}) {names[x]}")
                     valu=int(input())
                     play=players[valu]
-                    Captain.extortion(Ambassador,player,play)
+                    captain.extortion(ambassador,player,play)
                 if elec == 2:
-                    if Duke.block == True:
+                    if duke.block == True:
                         player.change_coins(2)
                     else:
-                        Duke.block =True
+                        duke.block =True
                 if elec == 5:
                     print('which player do you want to murder?')
                     for x in range(len(names)):
@@ -247,7 +247,7 @@ def main():
                             print(f"{x}) {names[x]}")
                     valu=int(input())
                     play=players[valu]
-                    Assassin.killer(player,Contessa,play,name_cards)
+                    assassin.killer(player,Contessa,play,name)
                 
 
 
